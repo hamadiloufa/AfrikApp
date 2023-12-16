2fe43acb642dd80d3ed01a773fb414ec05198064
hamadiloufa <fallouthiam219@gmail.com>
2023-12-16 17:25:19 +0000

    kick starter

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'AfrikApp',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
